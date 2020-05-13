8ba4d28dba5baf30b75deae19e260d98e7f33291
chavangorakh1999 <chavangorakh19999@gmail.com>
2020-05-13 21:28:43 +0530

    adding random color

--- personalexp/lib/widgets/transaction_item.dart
+++ personalexp/lib/widgets/transaction_item.dart
@@ -1,8 +1,10 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
-import '../models/transaction.dart';
-import 'package:intl/intl.dart';
 
-class TransactionItem extends StatelessWidget {
+import 'package:intl/intl.dart';
+import '../models/transaction.dart';
+
+class TransactionItem extends StatefulWidget {
   const TransactionItem({
     Key key,
     @required this.transaction,
@@ -11,6 +13,24 @@ class TransactionItem extends StatelessWidget {
 
   final Transaction transaction;
   final Function deleteTx;
+
+  @override
+  _TransactionItemState createState() => _TransactionItemState();
+}
+
+class _TransactionItemState extends State<TransactionItem> {
+  Color _bgColor;
+  @override
+  void initState() {
+    const availableColor = [
+      Colors.red,
+      Colors.purple,
+      Colors.green,
+      Colors.yellow
+    ];
+    _bgColor=availableColor[Random().nextInt(4)];
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -22,35 +42,35 @@ class TransactionItem extends StatelessWidget {
       ),
       child: ListTile(
         leading: CircleAvatar(
+          backgroundColor: _bgColor,
           radius: 30,
           child: Padding(
             padding: EdgeInsets.all(6),
             child: FittedBox(
-              child: Text('Rs ${transaction.amount}'),
+              child: Text('Rs ${widget.transaction.amount}'),
             ),
           ),
         ),
         title: Text(
-          transaction.tittle,
+          widget.transaction.tittle,
           style: Theme.of(context).textTheme.headline6,
         ),
         subtitle: Text(
-          DateFormat.yMMMd().format(transaction.date),
+          DateFormat.yMMMd().format(widget.transaction.date),
         ),
         trailing: MediaQuery.of(context).size.width > 460
             ? FlatButton.icon(
-                icon:const Icon(Icons.delete),
-                label:const Text('Delete'),
+                icon: const Icon(Icons.delete),
+                label: const Text('Delete'),
                 textColor: Theme.of(context).errorColor,
-                onPressed: () => deleteTx(transaction.id),
+                onPressed: () => widget.deleteTx(widget.transaction.id),
               )
             : IconButton(
                 icon: Icon(Icons.delete),
                 color: Theme.of(context).errorColor,
-                onPressed: () => deleteTx(transaction.id),
+                onPressed: () => widget.deleteTx(widget.transaction.id),
               ),
       ),
     );
   }
 }
-
